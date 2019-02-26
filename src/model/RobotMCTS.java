1e31c8aaea32138b3c0f4f65de56c3d09f5fc9b2
alexis barthelemy <alexis.barthelemy8@etu.univ-lorraine.fr>
2019-02-26 16:04:52 +0100

    Ajout generation de fils

--- src/model/RobotMCTS.java
+++ src/model/RobotMCTS.java
@@ -14,9 +14,14 @@ public class RobotMCTS extends Robot {
 	@Override
 	public Point jouer(int colonne, Plateau p) {
 		temps = System.currentTimeMillis();		
+		// On cree la racine de l'arbre 
+		Node racine = new Node(p);
+		
 		int nbOperation = 0;
 		while (System.currentTimeMillis() - temps < 3000) {
 			nbOperation++;
+			// On prend le noeud qui est terminal ou qui a des fils non developpé 
+			Node node = racine.nodeMax();
 			
 		}
 		System.out.println(nbOperation);
@@ -29,24 +34,34 @@ public class RobotMCTS extends Robot {
 		private ArrayList<Node> fils;
 		
 		public int signe;
+		private Plateau plateau;
 		
 		private final double C = Math.sqrt(2);
 		public int N = 0;
 		public double mu;
 		
-		public Node() {
+		public Node(Plateau p) {
 			signe = 1;
+			plateau = new Plateau(p);
 			fils = new ArrayList<Node>();
 		}
 		
-		public Node(Node parent) {
+		public Node(Plateau p, Node parent) {
 			this.parent = parent;
+			plateau = new Plateau(p);
 			fils = new ArrayList<Node>();
 			this.signe = parent.signe * -1;
 		}
 		
 		public void addFils(Node fils) {
 			this.fils.add(fils);
+		}
+		
+		public void genererFils() {
+			for (Integer indice : plateau.getIndicesColonnesJouables()) {
+				plateau.jouerColonne(indice);
+				
+			}
 		}
 		
 		public void update(double newProba) {
